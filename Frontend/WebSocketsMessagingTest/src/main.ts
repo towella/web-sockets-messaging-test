0939d06f4b891243d6fa8ef5b3fcfcf047f992f4
Andrew.Towell <Andrew.Towell@wisetechglobal.com>
2025-08-07 14:10:34 +1000

    Re-init project with typescript vue and asp.net core VS project (remnant folders from previous attempt under WebSocketsMessagingTest :(  )

--- Frontend/WebSocketsMessagingTest/src/main.ts
+++
@@ -1,8 +0,0 @@
-import '../styles/main.css';
-
-import { createApp } from 'vue';
-import App from './App.vue';
-
-const app = createApp(App);
-// app.component('comp-name', CompImportName);
-app.mount('#app');
