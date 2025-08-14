a054812cd894360611ae52ba4ca54951e9e8977e
Andrew.Towell <Andrew.Towell@wisetechglobal.com>
2025-08-14 18:04:07 +1000

    Added dist for realsies this time with git add <path> -f

--- WebSocketsMessagingTestApp/websocketsmessagingtestapp.client/vite.config.ts
+++ WebSocketsMessagingTestApp/websocketsmessagingtestapp.client/vite.config.ts
@@ -39,6 +39,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/web-sockets-messaging-test/',
     plugins: [plugin()],
     resolve: {
         alias: {
